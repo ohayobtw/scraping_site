bc2853cbb9153a5ae16d9728b35785d93ea87c0e
ohayobtw <gtavpro228@gmail.com>
2021-04-23 10:17:30 +0300

    first version

---
+++ scraping_site/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+import datetime
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'Pidor'
+    _context = {'name': name, 'date': date}
+    return render(request, 'home.html', _context)
